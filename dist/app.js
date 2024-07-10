a9a72bf240b755c1d6e06817787c148a1469a590
lucopdev <lucopdev@gmail.com>
2024-07-10 14:21:45 -0300

    cors configuration

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("@fastify/cors")); // Atualize a importação
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const accountRoutes_1 = __importDefault(require("./routes/accountRoutes"));
 const app = async (fastify) => {
+    fastify.register(cors_1.default, {
+        origin: '*', // Permite todas as origens. Para produção, substitua '*' por um URL específico ou uma lista de URLs permitidos.
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true, // Permite envio de cookies e outras credenciais
+    });
     fastify.get('/', async (request, reply) => {
         reply.send('Hello World!');
     });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import { FastifyInstance } from 'fastify';
+import FastifyCors from '@fastify/cors'; // Atualize a importação
 import userRoutes from './routes/userRoutes';
 import accountRoutes from './routes/accountRoutes';
 
 const app = async (fastify: FastifyInstance) => {
+  fastify.register(FastifyCors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   fastify.get('/', async (request, reply) => {
     reply.send('Hello World!');
   });
